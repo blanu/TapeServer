9a581ee600d8725c3ca14133c7d8619bd6f12be3
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2020-12-07 18:16:57 -0600

    Updated tests

--- Sources/TapeServer/TapeServer.swift
+++ Sources/TapeServer/TapeServer.swift
@@ -1,3 +1,36 @@
-struct TapeServer {
-    var text = "Hello, World!"
+import Foundation
+import Tape
+
+public class TapeServer
+{
+    var server: StreamServer?
+    var controllers: Set<StreamController> = Set()
+
+    init?()
+    {
+        let port = 1234
+        guard let server = StreamServer(port: port, callback: self.handleMessage) else {return nil}
+        self.server = server
+    }
+
+    public func handleMessage(controller: StreamController, message: Tape)
+    {
+        controllers.insert(controller)
+
+        switch message
+        {
+            case .play(_):
+                broadcast(message: message)
+            default:
+                print("Unhandled message: \(message)")
+        }
+    }
+
+    public func broadcast(message: Tape)
+    {
+        for controller in controllers
+        {
+            controller.send(tape: message)
+        }
+    }
 }
